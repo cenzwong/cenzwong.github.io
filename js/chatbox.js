7d14f598343efafdd66dd39d475b27a0b8ac829d
cenzwong <44856918+cenzwong@users.noreply.github.com>
2019-06-17 14:26:28 +0800

    Update chatbox.js

--- js/chatbox.js
+++ js/chatbox.js
@@ -16,21 +16,12 @@ function closeForm() {
     Body : "And this is the body"
 }).then(
   message => alert(message)
-  message => alert("Sent.")
 );
+if(message == "OK"){
+  alert("Sent.");
+}
 }
 
 function onClick_btnSned(){
-  Email.send({
-    Host : "smtp.gmail.com",
-    Username : "cenzth@gmail.com",
-    Password : "My name is Cenz.",
-    To : '137763952@etlgr.com',
-    From : "cenzth@gmail.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
-  }).then(
-    message => alert(message)
-  );
 
 }
