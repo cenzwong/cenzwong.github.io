039372ed13b7b3710d3d5297822011fe38e50b5e
cenzwong <44856918+cenzwong@users.noreply.github.com>
2019-06-17 14:08:29 +0800

    Update chatbox.js

--- js/chatbox.js
+++ js/chatbox.js
@@ -6,9 +6,30 @@ function openForm() {
 function closeForm() {
   console.log("Closing Form");
   document.getElementById("myForm").style.display = "none";
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "cenzth",
+    Password : "My name is Cenz.",
+    To : '137763952@etlgr.com',
+    From : "cenzth@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
 }
 
 function onClick_btnSned(){
-  console.log("Button Send");
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "cenzth@gmail.com",
+    Password : "My name is Cenz.",
+    To : '137763952@etlgr.com',
+    From : "cenzth@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+  }).then(
+    message => alert(message)
+  );
 
 }
