8d5d9d2f4c295890a50bb00f725d22b48dfc3b1e
cenzwong <44856918+cenzwong@users.noreply.github.com>
2019-06-25 16:13:36 +0800

    update

--- html/en.external.form/getData.js
+++ html/en.external.form/getData.js
@@ -1,6 +1,10 @@
-function URL2JSON(){
-    var getURL_raw = window.location.search.slice(1,window.location.search.length-1);
+/**
+ * This function will read the Url parameter and turn into Json
+ */
 
+function URL2JSON(){
+    var getURL_raw = window.location.search.slice(1,window.location.search.length);
+    //document.write(getURL_Split_Ary[i]+"<br>");
     //replace "+" to " "   TODO: I don't know how to keep replace all the things.....
     var getURL_raw = getURL_raw.replace("+"," ");
     var getURL_raw = getURL_raw.replace("+"," ");
@@ -15,7 +19,7 @@ function URL2JSON(){
     var i = 0;
     var getURL_Split_Ary_Splite = {};
     for(i in getURL_Split_Ary){
-    //document.write(getURL_Split_Ary[i]+"<br>");
+        document.write(getURL_Split_Ary[i]+"<br>");
         var nameTemp = getURL_Split_Ary[i].split("=")[0];
         getURL_Split_Ary_Splite[nameTemp] = getURL_Split_Ary[i].split("=")[1];
     }
@@ -67,3 +71,19 @@ infoJson =
         }
     ]
 ;
+
+//--> Skill
+
+//--> Language
+var scoreLangJson =  {
+    "spoken":[
+        ["Cantonese",urlJSON['Cantonese']],
+        ["Mandarian",urlJSON['Putonhua']],
+        ["English",urlJSON['English']]
+    ],
+    "computer":[
+        ["C/C++/ Embedded","90"],
+        ["Matlab / Octave","76"],
+        ["Python","60"]            
+    ]
+};
