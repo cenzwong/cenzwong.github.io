9f540f14fbea3a479d94ff6a1c911047c288539c
cenzwong <44856918+cenzwong@users.noreply.github.com>
2019-06-25 15:58:31 +0800

    Update getData.js

--- html/en.external.form/getData.js
+++ html/en.external.form/getData.js
@@ -1,47 +1,69 @@
-//get the whole URL down
-var getURL_raw = window.location.search.slice(1,window.location.search.length-1);
+function URL2JSON(){
+    var getURL_raw = window.location.search.slice(1,window.location.search.length-1);
 
-//replace "+" to " "   TODO: I don't know how to keep replace all the things.....
-var getURL_raw = getURL_raw.replace("+"," ");
-var getURL_raw = getURL_raw.replace("+"," ");
-var getURL_raw = getURL_raw.replace("+"," ");
-var getURL_raw = getURL_raw.replace("+"," ");
+    //replace "+" to " "   TODO: I don't know how to keep replace all the things.....
+    var getURL_raw = getURL_raw.replace("+"," ");
+    var getURL_raw = getURL_raw.replace("+"," ");
+    var getURL_raw = getURL_raw.replace("+"," ");
+    var getURL_raw = getURL_raw.replace("+"," ");
 
-//decode URI
-var getURL_raw = decodeURIComponent(getURL_raw);
+    //decode URI
+    var getURL_raw = decodeURIComponent(getURL_raw);
 
-var getURL_Split_Ary = getURL_raw.split("&");
+    var getURL_Split_Ary = getURL_raw.split("&");
 
-var i = 0;
-var getURL_Split_Ary_Splite = {};
-for(i in getURL_Split_Ary){
-  //document.write(getURL_Split_Ary[i]+"<br>");
-  var nameTemp = getURL_Split_Ary[i].split("=")[0];
-  getURL_Split_Ary_Splite[nameTemp] = getURL_Split_Ary[i].split("=")[1];
+    var i = 0;
+    var getURL_Split_Ary_Splite = {};
+    for(i in getURL_Split_Ary){
+    //document.write(getURL_Split_Ary[i]+"<br>");
+        var nameTemp = getURL_Split_Ary[i].split("=")[0];
+        getURL_Split_Ary_Splite[nameTemp] = getURL_Split_Ary[i].split("=")[1];
+    }
+    return getURL_Split_Ary_Splite;
 }
-// document.write(getURL_Split_Ary_Splite["Employee"]+"<br>");
-// document.write(getURL_Split_Ary_Splite["Home"]+"<br>");
-// document.write(getURL_Split_Ary_Splite["Email"]+"<br>");
-// document.write(getURL_Split_Ary_Splite["Phone"]+"<br>");
+
+var urlJSON = URL2JSON();
+// //get the whole URL down
+// var getURL_raw = window.location.search.slice(1,window.location.search.length-1);
+
+// //replace "+" to " "   TODO: I don't know how to keep replace all the things.....
+// var getURL_raw = getURL_raw.replace("+"," ");
+// var getURL_raw = getURL_raw.replace("+"," ");
+// var getURL_raw = getURL_raw.replace("+"," ");
+// var getURL_raw = getURL_raw.replace("+"," ");
+
+// //decode URI
+// var getURL_raw = decodeURIComponent(getURL_raw);
+
+// var getURL_Split_Ary = getURL_raw.split("&");
+
+// var i = 0;
+// var getURL_Split_Ary_Splite = {};
+// for(i in getURL_Split_Ary){
+//   //document.write(getURL_Split_Ary[i]+"<br>");
+//   var nameTemp = getURL_Split_Ary[i].split("=")[0];
+//   getURL_Split_Ary_Splite[nameTemp] = getURL_Split_Ary[i].split("=")[1];
+// }
+
 
 //-->
 infoJson = 
     [
         {
             "icon":"fa-briefcase",
-            "word":getURL_Split_Ary_Splite['Employee']
+            "word":urlJSON['Employee']
         },
         {
             "icon":"fa-home",
-            "word":getURL_Split_Ary_Splite['Home']
+            "word":urlJSON['Home']
         },
         {
             "icon":"fa-envelope",
-            "word":"<a href=\"mailto:"+getURL_Split_Ary_Splite['Email']+"\">"+getURL_Split_Ary_Splite['Email']+"</a>"
+            "word":"<a href=\"mailto:"+urlJSON['Email']+"\">"+urlJSON['Email']+"</a>"
         },
         {
             "icon":"fa-phone",
-            "word":getURL_Split_Ary_Splite['Phone']
+            "word":urlJSON['Phone']
         }
     ]
 ;
