3a4703af427a9bb59e6ac3d3f8c4eb71a9aad832
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-01-20 00:05:34 +0800

    Update dropdownIntro.js

--- dropdownIntro.js
+++ dropdownIntro.js
@@ -2,7 +2,7 @@ var DropdownData = [
      {
         "Year" : "2020",
         "Time":["Jan"],
-        "Activities/Experience":[   "<a href='https://courses.nvidia.com/certificates/5baa4dcb2bce4d8a80bd73303cee6e63/' target='_blank'>GETTING STARTED WITH AI ON JETSON NANO</a> @ NVIDIA Deep Learning Institute"]
+        "Activities/Experience":[   "<a href='https://courses.nvidia.com/certificates/5baa4dcb2bce4d8a80bd73303cee6e63' target='_blank'>Getting Started with AI on Jetson Nano</a> @ NVIDIA Deep Learning Institute"]
     },
     {
         "Year" : "2019",
