69ee286b38a3edb93ced693f7d2e73c56d0ff5e0
Cenz Wong <cenzth@gmail.com>
2020-03-29 11:07:22 +0000

    updated

--- dropdownIntro.js
+++ dropdownIntro.js
@@ -2,7 +2,8 @@ var DropdownData = [
      {
         "Year" : "2020",
         "Time":["Jan"],
-        "Activities/Experience":[   "<a href='https://courses.nvidia.com/certificates/5baa4dcb2bce4d8a80bd73303cee6e63' target='_blank'>Getting Started with AI on Jetson Nano</a> @ NVIDIA Deep Learning Institute"]
+        "Activities/Experience":[   "<a href='https://courses.nvidia.com/certificates/5baa4dcb2bce4d8a80bd73303cee6e63' target='_blank'>Getting Started with AI on Jetson Nano</a> @ NVIDIA Deep Learning Institute",
+                                    "FINSPIRE Hackathon"    ]
     },
     {
         "Year" : "2019",
